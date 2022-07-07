e1166ba189064c462b806b513fbd25c452e3b946
khorshedHasanTutul <khasan7890@gmail.com>
2022-07-07 16:57:25 +0600

    Services Base Work

---
+++ Client/Services/ProductService/ProductService.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EcommerceWebAsmb.Shared;
+using System.Net.Http.Json;
+
+namespace EcommerceWebAsmb.Client.Services.ProductService
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _http;
+        public ProductService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public List<Product> Products { get; set; } = new List<Product>();
+
+        public async Task GetProducts()
+        {
+            var response = await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("/api/Product");
+            if (response != null && response.Data != null)
+            {
+                Products = response.Data;
+            }
+        }
+    }
+}
--- Server/Controllers/ProductController.cs
+++ Server/Controllers/ProductController.cs
@@ -1,23 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using EcommerceWebAsmb.Server.Data;
 using Microsoft.EntityFrameworkCore;
+using EcommerceWebAsmb.Server.Services.ProductService;
 
 namespace EcommerceWebAsmb.Server.Controllers
 {
     public class ProductController : BaseController
     {
-        private readonly DataContext _context;
+        private readonly IProductService _productService;
         private static List<Product> Products = new List<Product>();
 
-        public ProductController(DataContext context)
+        public ProductController(IProductService productService)
         {
-            _context = context;
+            _productService = productService;
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts()
+        public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProducts()
         {
-            var Products = await _context.Products.ToListAsync();
+            var Products = await _productService.GetServiceResponseAsync();
             return Ok(Products);
         }
     }
